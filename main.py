0c565f1f87925e2a9244663282b414dbfc62c57b
srijanisaDev <srijankumar770@gmail.com>
2026-01-23 22:28:46 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+import json
+
+app = FastAPI()
+
+def load_data():
+    with open('patients.json','r') as f:
+        data = json.load(f)
+
+        return data
+
+
+@app.get("/")
+def hello():
+    return {'message' : 'Patient Management System API'}
+
+@app.get("/about")
+def about():
+    return {'message' : ' A fully functional API to manage your patient records'}
+
+@app.get("/view")
+def view():
+    data = load_data()
+
+    return data
+
+@app.get('/patient/{patient_id}')
+def view_patient(patient_id : str):
+    # load all the patients
+    data = load_data()
+
+    if patient_id in data:
+        return data[patient_id]
+    return {'error' : 'patient doest not exist'}
