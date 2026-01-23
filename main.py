0354e144a5f6de68f1173c535a0b985bb42eda44
srijanisaDev <srijankumar770@gmail.com>
2026-01-24 02:08:03 +0530

    leart about Query parameters

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , Path , HTTPException , Query
 import json
 
 app = FastAPI()
@@ -25,10 +25,30 @@ def view():
     return data
 
 @app.get('/patient/{patient_id}')
-def view_patient(patient_id : str):
-    # load all the patients
+def view_patient(patient_id : str = Path(..., description = 'ID of the patient in the DB',example='P001')):
+    
     data = load_data()
 
     if patient_id in data:
         return data[patient_id]
-    return {'error' : 'patient doest not exist'}
+    raise HTTPException(status_code=404,detail='Patient not found!!')
+
+@app.get("/sort")
+def sort_patients(sort_by: str = Query(..., description='Sort on the basis of height, weight or bmi'),
+                  order: str = Query('asc', description='sort in asc or desc order')):
+    
+    valid_fields = ['height', 'weight', 'bmi']
+    
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400, detail='Invalid field select from {valid_fields}')
+    
+    if order not in ['asc', 'desc']:
+        raise HTTPException(status_code=400, detail='Invalid order select from between asc and desc')
+    
+    data = load_data()
+    
+    sort_order = True if order == 'desc' else False
+    
+    sorted_data = sorted(data.values(), key=lambda x: x.get(sort_by, 0), reverse=sort_order)
+    
+    return sorted_data
